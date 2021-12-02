50e8ed45a748df5203996b2c35e88f3fb7b1c5a7
ellipse120 <ellipse120@gmail.com>
2021-12-02 16:08:51 +0800

    refactor: use default publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@
  * @type {import('@vue/cli-service').ProjectOptions}
  */
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/production-sub-path/'
-        : '/',
     pages: {
         index: {
             entry: 'src/app/main.js',
